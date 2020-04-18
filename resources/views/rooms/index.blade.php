24e9cfe40afa4f91cedc02116af2405f2511775a
jos3duardo <jos3duardolopes@gmail.com>
2020-04-17 22:30:33 -0400

    criando rotas das salas e mostrando as salas disponiveis

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -2,7 +2,19 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col s12 m12 l12 of">
+            <div class="col s12 m12 l12 offset-m2">
+                <div class="card-panel">
+                    <div class="title">Escolha uma das salas abaixo</div>
+                    <div class="card-content">
+                        @foreach($rooms as $room)
+                            <ul class="collection">
+                                <li class="collection-item">
+                                    <a href="{{route('chat.rooms.show',['room' => $room->id])}}">{{ $room->name }}</a>
+                                </li>
+                            </ul>
+                        @endforeach
+                    </div>
+                </div>
 
             </div>
         </div>
