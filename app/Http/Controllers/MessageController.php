ae1c7da82e6599f90e6ae83f6ac8a1af536dc977
jos3duardo <jos3duardolopes@gmail.com>
2020-04-20 20:54:40 -0400

    remove methods not used

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -36,27 +36,7 @@ class MessageController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
 
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Message  $message
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Message $message)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
