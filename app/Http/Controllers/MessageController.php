f936cf38d24b9aeb48972760791e16e5a4324b9e
jos3duardo <jos3duardolopes@gmail.com>
2020-04-20 20:45:59 -0400

    remove classes not use

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\SendMessage;
 use App\Models\Message;
-use App\Models\Room;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
